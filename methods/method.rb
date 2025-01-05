cc4b2118e5686f9b06af138fae9baa4f22dbdb28
Muash <muazwazir@gmail.com>
2025-01-06 00:27:38 +0800

    methods in ruby

--- methods/method.rb
+++ methods/method.rb
@@ -1,5 +1,33 @@
 # frozen_string_literal: true
 
-def calculate
-  puts 'method to calculate'.upcase
+def say(name)
+  puts "hello world, #{name}"
 end
+
+# setting default value
+def say_with_default(name = '')
+  puts "hello world, #{name}"
+end
+
+def say_with_keyowrd_arguments(greeting:, name:)
+  puts "#{greeting}, #{name}"
+end
+
+# have to explicitely put the argument names and values
+say_with_keyowrd_arguments(greeting: 'fucku', name: 'elon')
+# order doesn't matter with explicit keyword arguments
+say_with_keyowrd_arguments(name: 'elon', greeting: 'fucku')
+
+def say_with_keyowrd_arguments_and_default_value(greeting: '', name: '')
+  puts "#{greeting}, #{name}"
+end
+
+say_with_keyowrd_arguments_and_default_value(name: 'sheldon', greeting: 'where is ur dad sheldon')
+
+def return_type
+  puts 'hello'
+  2 + 2 # the final statement evaluated is the one being return, this is an implicit return
+end
+
+result = return_type
+puts result
